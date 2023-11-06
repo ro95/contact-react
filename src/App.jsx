fb5186f6c30c622ec39338d1487bbdaa47a88293
Rodolph <rodolphcopr@gmail.com>
2023-11-06 17:53:12 +0100

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Form from "./Components/Form";
+
+const App = () => (
+	<div className="contenaire">
+		<h1>Bouygues telecom entreprise plane</h1>
+		<Form />
+	</div>
+);
+
+export default App;
