4efda07d70ebf9fa7665fca1e6e06db4df385995
Rodolph <rodolphcopr@gmail.com>
2023-11-10 12:25:54 +0100

    for do

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Ticket from "./Components/ticket/Ticket";
 import Form from "./Components/Form";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => (
-	<div>
-		<Ticket />
-	</div>
+	<Router>
+		<Routes>
+			<Route path="/" element={<Form />} />
+			{/* other routes */}
+		</Routes>
+	</Router>
 );
 
 export default App;
